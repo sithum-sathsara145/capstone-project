4e855cf3ac9101e2d0ed1cb677dc93f7f19f8017
sithum-sathsara145 <sithumsathsara145@gmail.com>
2023-05-04 14:34:03 +0530

    making reservation form

--- capstone/src/components/Reservations.js
+++ capstone/src/components/Reservations.js
@@ -1,7 +1,50 @@
+import React from "react"
+
 function Reservations(){
+    //let today = new Date()
+    const [time,setTime]= React.useState('18:00');
+    const [date,setDate] = React.useState();
+    const [guests,setGuests] = React.useState('1')
+    const [occasion,setOccasion] = React.useState()
+    const eventhandler = (e)=>{
+        e.preventDefault()
+        alert('form submitted')
+    }
+    const  availableTimes = ["17:00",'18:00',"19:00","20:00",'21:00','22:00']
+    const timeslots = availableTimes.map((times)=> {return <option>{times}</option>})
     return(
-        <div className="hero">
-            <h1>reserved</h1>
+        <div className="reserve">
+            <h1 style={{textAlign: "center"}}>
+                Welcome to Table Reservation system on Little lemon
+            </h1>
+            <form>
+                <label htmlFor="res-date">Choose Date</label>
+                <input type="date" id="res-date" name="res-date" value={date} onChange={(e)=>{return setDate(e.target.value)}}/>
+                <label htmlFor="res-time">Choose time</label>
+                <select id="res-time " value={time} onChange={(e)=>{return setTime(e.target.value)}}>
+                {timeslots}
+                </select>
+                <label htmlFor="noguests">No of Guests</label>
+                <input
+                type="number"
+                id="noguests"
+                name="noguests"
+                min={1} max={10}
+                value={guests}
+                onChange={(e)=>{
+                    return setGuests(e.target.value)
+                }}/>
+                <label htmlFor="occasion">Occasion</label>
+                <select 
+                id="occasion" 
+                name="occasion" 
+                value={occasion} 
+                onChange={(e) => { return setOccasion(e.target.value)}}>
+                    <option>Annivesary</option>
+                    <option>Birth day</option>
+                </select>
+                <button type="submit" onClick={eventhandler}>make reservation</button>
+            </form>
         </div>
     )
 }
